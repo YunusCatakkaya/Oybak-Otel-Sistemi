28f052c7542ec29627cb2f192c332281d85077e9
onuro <onuro@192.168.1.2>
2026-05-06 14:31:14 +0300

    tc kimlik no çift haneyle bitmeli durumu düzenlendi.

--- src/main/java/com/oybak/otel/Hatalar.java
+++ src/main/java/com/oybak/otel/Hatalar.java
@@ -35,7 +35,7 @@ public interface Hatalar {
                 toplam = toplam + hane;
             }
             
-            if((toplam%10) != sonHane){
+            if(((toplam%10) != sonHane) || ((sonHane%2) !=0)){
                 JOptionPane.showMessageDialog((Component) this, "Geçerli bir TC no giriniz!");
                 return false;
             }
