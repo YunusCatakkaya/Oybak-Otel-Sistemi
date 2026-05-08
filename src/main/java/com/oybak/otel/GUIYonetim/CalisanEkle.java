e919cf30187267815e777847c70972e39b78cfc9
Yunus <Yunus@172.27.63.246>
2026-05-08 23:24:09 +0300

    efe için deneme

--- src/main/java/com/oybak/otel/GUIYonetim/CalisanEkle.java
+++ src/main/java/com/oybak/otel/GUIYonetim/CalisanEkle.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.oybak.otel.GUIYonetim;
+package com.oybak.otel.GUIYonetim;//8
 import com.oybak.otel.Yonetim;
 import com.oybak.otel.enums.UserRole;
 import com.oybak.otel.Personel;
